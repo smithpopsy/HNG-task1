e29889c638f1dd89faea092d11a867683019ce07
SMITH <olumidesmith17@gmail.com>
2023-09-08 16:23:04 +0100

    last

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to update the current day of the week
+
 function updateCurrentDay() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDate = new Date();
@@ -6,15 +6,11 @@ function updateCurrentDay() {
     const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     currentDayElement.textContent = `Current day of the week: ${currentDay}`;
 }
-
-// Function to update the current UTC time
 function updateCurrentUTCTime() {
     const currentDate = new Date();
-    const currentUTCTimeMilliseconds = currentDate.getTime(); // Get time in milliseconds since the Unix epoch
+    const currentUTCTimeMilliseconds = currentDate.getTime();
     const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
     currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTimeMilliseconds}`;
 }
-
-// Call the update functions on page load
 updateCurrentDay();
 updateCurrentUTCTime();
